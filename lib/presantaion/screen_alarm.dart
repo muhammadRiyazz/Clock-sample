9e8922b0e5c8d65f7ab66a57bc8497eb04a02ce8
Muhammad Riyaz <riyazmuhammad786rz@gmail.com>
2023-02-09 22:11:07 +0530

    alarm screen created

---
+++ lib/presantaion/screen_alarm.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ScreenAlarm extends StatelessWidget {
+  const ScreenAlarm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
